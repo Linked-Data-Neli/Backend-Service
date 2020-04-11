2f9f72fe04d5fd96b6cb7d5162c9d910edff8ea1
AdrianAlucard <wartinator@gmail.com>
2020-04-11 15:24:39 -0400

    added global config for queryService dependency injection

--- src/main/java/com/linkedata/univ/jenaproject/JenaProjectApplication.java
+++ src/main/java/com/linkedata/univ/jenaproject/JenaProjectApplication.java
@@ -1,7 +1,11 @@
 package com.linkedata.univ.jenaproject;
 
+import com.linkedata.univ.jenaproject.services.QueryService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.management.Query;
 
 @SpringBootApplication
 public class JenaProjectApplication {
@@ -10,4 +14,8 @@ public class JenaProjectApplication {
         SpringApplication.run(JenaProjectApplication.class, args);
     }
 
+    @Bean()
+    public QueryService queryService() {
+        return QueryService.buildOwlQueryService("hw2.ttl"); // insert fileName here -- add file to resources folder
+    }
 }
